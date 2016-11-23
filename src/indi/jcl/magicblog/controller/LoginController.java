c5c35de8c754a3ee4db56cfa26a745b0796514cc
Mag1cLong <147263180@qq.com>
2016-11-23 17:22:01 +0800

    aop日志

--- src/indi/jcl/magicblog/controller/LoginController.java
+++ src/indi/jcl/magicblog/controller/LoginController.java
@@ -1,9 +1,11 @@
 package indi.jcl.magicblog.controller;
 
+import indi.jcl.magicblog.service.IUserService;
 import indi.jcl.magicblog.vo.Response;
 import indi.jcl.magicblog.vo.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,7 +21,6 @@ public class LoginController {
 	@RequestMapping(value = "login")
 	@ResponseBody
 	public Response login( String userName,String pwd,HttpSession session)throws Exception{
-		logger.info("user:"+userName+" ---> login......");
 		if(StringUtils.isEmpty(userName)||StringUtils.isEmpty(pwd)){
 			return new Response(Response.FAIL,"用户名或密码不能为空");
 		}
--- src/indi/jcl/magicblog/interceptor/PermissionInterceptor.java
+++ src/indi/jcl/magicblog/interceptor/PermissionInterceptor.java
@@ -1,7 +1,5 @@
 package indi.jcl.magicblog.interceptor;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.util.StringUtils;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
@@ -14,10 +12,8 @@ import javax.servlet.http.HttpSession;
  * Created by Administrator on 2016/9/22.
  */
 public class PermissionInterceptor extends HandlerInterceptorAdapter {
-    private static final Logger logger = LoggerFactory.getLogger(PermissionInterceptor.class);
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        logger.info("PermissionInterceptor......preHandle");
         HttpSession session = request.getSession(true);
         String requestType = request.getHeader("X-Requested-With");
         if (session.getAttribute("session") == null) {
@@ -35,13 +31,11 @@ public class PermissionInterceptor extends HandlerInterceptorAdapter {
 
     @Override
     public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
-        logger.info("PermissionInterceptor......postHandle");
         super.postHandle(request, response, handler, modelAndView);
     }
 
     @Override
     public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
-        logger.info("PermissionInterceptor......afterCompletion");
         super.afterCompletion(request, response, handler, ex);
     }
 }
